0d00db82defcfaf62f5ef234f290ec5fd56594b6
Ana-Maria <ppe.anamaria@gmail.com>
2017-05-09 11:58:46 +0300

    Update functia_random.py

--- MPI/functia_random.py
+++ MPI/functia_random.py
@@ -1,7 +1,7 @@
 import random
 
 
-mylist1 = []            # elemente random
+mylist1 = []            # elemente random 10.000
 for i in range(10000):
     i = random.randrange(1,10000)
     mylist1.append(i)
@@ -9,11 +9,11 @@ with open('list1.txt', 'w') as f:
     for i in mylist1:
         f.write("%d " %i)
     f.close()
-
-
-mylist2 = []               # elemente random
-for i in range(500000):
-    i = random.randrange(1,5000000)
+    
+    
+mylist2 = []            # elemente random 200.000
+for i in range(200000):
+    i = random.randrange(1,2000000)
     mylist2.append(i)
 with open('list2.txt', 'w') as f:
     for i in mylist2:
@@ -21,9 +21,9 @@ with open('list2.txt', 'w') as f:
     f.close()
 
 
-mylist3 = []                # elemente random
-for i in range(5000000):
-    i = random.randrange(1,50000000)
+mylist3 = []               # elemente random 500.000
+for i in range(500000):
+    i = random.randrange(1,5000000)
     mylist3.append(i)
 with open('list3.txt', 'w') as f:
     for i in mylist3:
@@ -31,9 +31,9 @@ with open('list3.txt', 'w') as f:
     f.close()
 
 
-mylist4 = []                # elemente random
-for i in range(20000000):
-    i = random.randrange(1,200000000)
+mylist4 = []                # elemente random 5.000.000
+for i in range(5000000):
+    i = random.randrange(1,50000000)
     mylist4.append(i)
 with open('list4.txt', 'w') as f:
     for i in mylist4:
@@ -41,37 +41,56 @@ with open('list4.txt', 'w') as f:
     f.close()
 
 
-mylist5 = []                # lista ordonata crescator
-for i in range(200000):
-    i = random.randrange(1,2000000)
+mylist5 = []                # elemente random 20.000.000
+for i in range(20000000):
+    i = random.randrange(1,200000000)
     mylist5.append(i)
-mylist5.sort()
 with open('list5.txt', 'w') as f:
     for i in mylist5:
         f.write("%d " %i)
     f.close()
 
-
-mylist6 = []      # lista ordonata descrescator
-for i in range(5000000):
-    i = random.randrange(1,50000000)
+    
+mylist6 = []               # elemente random 200.000.000
+for i in range(200000000):
+    i = random.randrange(1,2000000000)
     mylist6.append(i)
-mylist6.sort(reverse=True)
 with open('list6.txt', 'w') as f:
     for i in mylist6:
         f.write("%d " %i)
     f.close()
+   
+mylist7 = []                  #lista ordonata crescator
+for i in range(200000):
+    i = random.randrange(1,2000000)
+    mylist7.append(i)
+mylist7.sort()
+with open('list7.txt', 'w') as f:
+    for i in mylist7:
+        f.write("%d " %i)
+    f.close()
 
 
-mylist7 = []        # lista ordonata crescator avand doar cateva elemente care nu sunt la locul lor
-for i in range(1,5000000):
+mylist8 = []      # lista ordonata descrescator cu 200.000 elemente
+for i in range(200000):
+    i = random.randrange(1,2000000)
+    mylist8.append(i)
+mylist8.sort(reverse=True)
+with open('list8.txt', 'w') as f:
+    for i in mylist8:
+        f.write("%d " %i)
+    f.close()
+
+
+mylist9 = []        # lista ordonata crescator avand doar cateva elemente care nu sunt la locul lor cu 200.000 elemente
+for i in range(1,200000):
     if i % 10000 == 0:
         for j in range(2):
             j = random.randrange(100,200)
-            mylist7.append(j)
+            mylist9.append(j)
     else:
-        mylist7.append(i)
-with open('list7.txt', 'w') as f:
-    for i in mylist7:
+        mylist9.append(i)
+with open('list9.txt', 'w') as f:
+    for i in mylist9:
         f.write("%d " %i)
     f.close()
